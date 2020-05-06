a981ef870684af684834e6e1c750540f177dc534
Krystal Guzman <kryguzman@gmail.com>
2020-05-06 16:50:41 -0700

    updated databse

--- data/migrations/20200331083334_tables.js
+++ data/migrations/20200331083334_tables.js
@@ -15,10 +15,9 @@ exports.up = function(knex) {
         tbl.increments();
         tbl.string('frontCard').notNullable();
         tbl.string('backCard').notNullable();
-        tbl.string('category').notNullable()
+        tbl.boolean('cardTesting').notNullable().defaultTo(false);
     })
     .createTable('user-categories', (tbl) => {
-      tbl.increments();
       tbl
           .integer('user_id')
           .unsigned()
@@ -35,12 +34,58 @@ exports.up = function(knex) {
           .inTable('categories')
           .onDelete('RESTRICT')
           .onUpdate('CASCADE');
-      tbl.boolean('categoryTesting').notNullable().defaultTo(false);
+      tbl
+          .boolean('categoryTesting')
+          .notNullable()
+          .defaultTo(false);
+  })
+  .createTable('mastery', (tbl) => {
+    tbl
+          .integer('flashcard_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('flashcards')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      tbl
+          .integer('category_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('categories')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      tbl
+          .integer('user_id')
+          .unsigned()
+          .notNullable()
+          .references('id')
+          .inTable('users')
+          .onDelete('RESTRICT')
+          .onUpdate('CASCADE');
+      tbl
+          .integer('mastery')
+          .notNullable()
+          .defaultTo(0);
+      tbl
+          .integer('countUp')
+          .notNullable()
+          .defaultTo(0);
+      tbl
+          .integer('countDown')
+          .notNullable() 
+          .defaultTo(0);
+        tbl
+        .boolean('last')
+        .notNullable()
+        .defaultTo(false);
   })
   };
   
   exports.down = function(knex) {
     return knex.schema
+    .dropTableIfExists("mastery")
     .dropTableIfExists("user-categories")
     .dropTableIfExists("flashcards")
     .dropTableIfExists("categories")
